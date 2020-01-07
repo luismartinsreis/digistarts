6a55e63af3ea4a1666661c0602b98fa591edcc09
luis <luis.martins.reis@outlook.com>
2020-01-06 21:24:14 -0300

    resolucao problema 1 - conjuntos

---
+++ p1.py
@@ -0,0 +1,18 @@
+
+
+def unique():
+    with open("input1.txt", "r") as fin:
+        y=set(fin.readlines()[1:])
+        unique_set = sorted(set(map(int, y)))
+        [print(k) for k in unique_set if -1000<= k <=10000]
+
+        fin.seek(0)
+        n=int(fin.readline())
+        if 1 <= n <=1000:
+            pass
+        else:
+            print('número {} fora da faixa permitida: 1 <= n <=1000'.format(n))
+
+
+if __name__ == "__main__":
+    unique()
